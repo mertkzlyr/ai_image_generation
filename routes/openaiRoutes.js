426c2a2247564220d605fbf69e5b15abc77dffbb
mertkzlyr <91737596+mertkzlyr@users.noreply.github.com>
2024-05-05 22:18:36 +0300

    editImage function added

--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -1,9 +1,10 @@
 // Importing required modules
 const express = require('express');
-const { generateImage } = require('../controllers/openaiController'); // Importing the controller function
+const { generateImage, editImage } = require('../controllers/openaiController'); // Importing the controller function
 const router = express.Router();
 
 // Define routes
 router.post('/generateimage', generateImage); // Handle POST requests to generateimage endpoint
+router.post('/editimage', editImage);
 
 module.exports = router; // Export router
